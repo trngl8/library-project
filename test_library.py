05dbaf41e1b2d9e6d6f8133589733dbfc59b07f3
Red Rubber Duck <mvch@triangle.software>
2023-06-22 19:54:41 +0300

    syntax updates

--- test_library.py
+++ test_library.py
@@ -10,24 +10,25 @@ class TestLibrary(unittest.TestCase):
     A reader is able to order a book
     """
 
-    def test_are_books(self):
+    def testLibraryHasBooks(self):
         target = Library()
         result = target.get_count()
         self.assertEqual(result, 100)
 
-    def test_visitor(self):
+    def testLibraryConvenient(self):
         visitor = User()
         target = Library()
         result = visitor.availableLibrary(target)
         self.assertEqual(True, result)
 
-    def test_user(self):
+    def testUserBook(self):
         user = User()
         book = Book()
         library = Library()
         library.addBook(book)
         user.orderBook(book)
         self.assertEqual(True, user.hasBooks())
+
 
 if __name__ == "__main__":
     unittest.main()

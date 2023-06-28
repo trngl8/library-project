7b0f5cb6959e06c3d8d938d127b21a32b101f051
Red Rubber Duck <mvch@triangle.software>
2023-06-28 22:03:21 +0300

    name input for users, close #36

--- index.py
+++ index.py
@@ -1,8 +1,10 @@
 from flask import Flask
 from flask import render_template
+from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
 
+bootstrap = Bootstrap5(app)
 
 @app.route('/')
 def index():

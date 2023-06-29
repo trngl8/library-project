f063547b16ad77e5b9fced462a98a8658fd5039a
Red Rubber Duck <mvch@triangle.software>
2023-06-29 13:05:58 +0300

    added detailed test

--- test_library.py
+++ test_library.py
@@ -43,20 +43,18 @@ class TestLibrary(unittest.TestCase):
 
     def test_import_books(self):
         library = Library()
+        book1 = Book("Python Crash Course", "Eric Matthes", 2019)
+        book2 = Book("Python Hard Way", "Zed Shaw", 2019)
+        book3 = Book("Head First Python", "Paul Barry", 2019)
         library.import_books([
             ["Python Crash Course", "Eric Matthes", 2019],
             ["Python Hard Way", "Zed Shaw", 2013],
             ["Head First Python", "Paul Barry", 2016]
         ])
         self.assertEqual(3, library.get_count())
-
-    def test_book(self):
-        book = Book("Python Crash Course", "Eric Matthes", 2019)
-        self.assertEqual("Python Crash Course", book.title)
-        self.assertEqual("Eric Matthes", book.author)
-        self.assertEqual(2019, book.year)
-        self.assertEqual(False, book.available)
-        self.assertEqual(0, book.count)
+        self.assertEqual(book1, library.catalog[0])
+        self.assertEqual(book2, library.catalog[1])
+        self.assertEqual(book3, library.catalog[2])
 
 
 if __name__ == "__main__":

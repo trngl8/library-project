ff47fc7a899c40157d09f615115be73e909f9624
Red Rubber Duck <mvch@triangle.software>
2023-06-30 19:08:50 +0300

    fix import

--- library.py
+++ library.py
@@ -7,41 +7,42 @@ class Library:
 
     def get_count(self):
         return len(self.catalog)
-    
-    def add_book(self , book):
+
+    def add_book(self, book):
         self.catalog.append(book)
-    
-    def import_books(self , lst_of_books):
-        for i in lst_of_books:
-            self.add_book(Book(lst_of_books[0], lst_of_books[1], lst_of_books[2]))
-    
+
+    def import_books(self, list_of_books):
+        for item in list_of_books:
+            self.add_book(Book(item[0], item[1], item[2]))
+
     @staticmethod
     def read_from_csv_catalog(path):
-        with open(path , 'r') as file:
+        with open(path, 'r') as file:
             reader = csv.reader(file)
             reader = list(reader)
-         
+
+
 class User:
-    def __init__(self , wanted_amount = 10) -> None:
+    def __init__(self, wanted_amount=10) -> None:
         self.books = []
         self.wanted_amount = wanted_amount
 
-    def available_library(self , library : Library):
+    def available_library(self, library: Library):
         if library.get_count() >= self.wanted_amount:
             return True
         return False
 
-    def order_book(self , book):
+    def order_book(self, book):
         self.books.append(book)
-    
+
     def has_books(self):
         if len(self.books) > 0:
             return True
         return False
-    
+
 
 class Book:
-    def __init__(self, title, author, year, ISBN = None) -> None:
+    def __init__(self, title, author, year, ISBN=None) -> None:
         self.isbn = ISBN
         self.title = title
         self.author = author
@@ -49,10 +50,7 @@ class Book:
         self.available = False
         self.count = 0
 
-
     def __eq__(self, anohter) -> bool:
         if self.title == anohter.title and self.author == anohter.author and self.year == anohter.year:
             return True
         return False
-
-

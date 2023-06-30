8f6ad5994c36ee52c27910532a33f85a40574be4
ArtemKrayevskiy <kraievskyi.pn@ucu.edu.ua>
2023-06-30 17:54:16 +0300

    Resolved

--- functions.py
+++ functions.py
@@ -37,5 +37,39 @@ user.ask_order(book2)
 user.borrow(book2)
 
 
+def create_csv(path):
+    column_names = ['id' , 'ISBN' , "Titile" , "Author", "Year", "Available", "Count"]
+    with open(path , 'w') as file:
+        writer = csv.writer(file)
+        writer.writerow(column_names)
 
+def add_book_to_csv(book : Book , path):
+    if  not check_if_book_in_csv(path = path, book=book)[0]:
+        with open(path, 'r') as file:
+            reader = csv.reader(file)
+            data = list(reader)
+        data.append([book.title, book.author,\
+                            book.year, book.available, book.count])
+        with open(path , 'w') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+    elif check_if_book_in_csv(path = path, book = book)[0]:
+        row_number = check_if_book_in_csv(path = path, book = book)[1]
+        with open(path, 'r') as file:
+            reader = csv.reader(file)
+            data = list(reader)
+        data[row_number][4] = int(data[row_number][4])
+        data[row_number][4] += 1
+        with open(path , 'w') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+
+def check_if_book_in_csv(path , book):
+    with open(path , 'r') as file:
+        reader = csv.reader(file)
+        reader = list(reader)
+        for number_of_row , row in enumerate(reader):
+            if book == Book(row[0], row[1], row[2], row[3], row[4]):
+                return True , number_of_row
+        return False , 0
 
--- library.py
+++ library.py
@@ -12,7 +12,8 @@ class Library:
         self.catalog.append(book)
     
     def import_books(self , lst_of_books):
-        self.catalog = lst_of_books
+        for i in lst_of_books:
+            self.add_book(Book(lst_of_books[0], lst_of_books[1], lst_of_books[2]))
     
     @staticmethod
     def read_from_csv_catalog(path):
@@ -40,7 +41,8 @@ class User:
     
 
 class Book:
-    def __init__(self, title = None, author = None, year = 0 , available = False, count = 0) -> None:
+    def __init__(self, title, author, year, ISBN = None) -> None:
+        self.isbn = ISBN
         self.title = title
         self.author = author
         self.year = year
@@ -49,42 +51,8 @@ class Book:
 
 
     def __eq__(self, anohter) -> bool:
-        if self.title == anohter.title and self.author == anohter.author:
+        if self.title == anohter.title and self.author == anohter.author and self.year == anohter.year:
             return True
         return False
 
-# def create_csv(path):
-#     column_names = ['id' , 'ISBN' , "Titile" , "Author", "Year", "Available", "Count"]
-#     with open(path , 'w') as file:
-#         writer = csv.writer(file)
-#         writer.writerow(column_names)
 
-# def add_book_to_csv(book : Book , path):
-#     if  not check_if_book_in_csv(path = path, book=book)[0]:
-#         with open(path, 'r') as file:
-#             reader = csv.reader(file)
-#             data = list(reader)
-#         data.append([book.title, book.author,\
-#                             book.year, book.available, book.count])
-#         with open(path , 'w') as file:
-#             writer = csv.writer(file)
-#             writer.writerows(data)
-#     elif check_if_book_in_csv(path = path, book = book)[0]:
-#         row_number = check_if_book_in_csv(path = path, book = book)[1]
-#         with open(path, 'r') as file:
-#             reader = csv.reader(file)
-#             data = list(reader)
-#         data[row_number][4] = int(data[row_number][4])
-#         data[row_number][4] += 1
-#         with open(path , 'w') as file:
-#             writer = csv.writer(file)
-#             writer.writerows(data)
-
-# def check_if_book_in_csv(path , book):
-#     with open(path , 'r') as file:
-#         reader = csv.reader(file)
-#         reader = list(reader)
-#         for number_of_row , row in enumerate(reader):
-#             if book == Book(row[0], row[1], row[2], row[3], row[4]):
-#                 return True , number_of_row
-#         return False , 0

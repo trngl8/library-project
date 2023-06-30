5dd6f713648b49310727bc6ea02246bb84670ae4
Red Rubber Duck <mvch@triangle.software>
2023-06-30 22:15:18 +0300

    index page, close #17

--- index.py
+++ index.py
@@ -2,14 +2,29 @@ from flask import Flask
 from flask import render_template
 from flask_bootstrap import Bootstrap5
 
+from library import Library
+from library import Book
+
 app = Flask(__name__)
 
 bootstrap = Bootstrap5(app)
+
 
 @app.route('/')
 def index():
     name = 'Library "3 Books"'
     return render_template('enter.html', name=name)
+
+
+@app.route('/index')
+def catalog():
+    name = 'Library "3 Books"'
+    library = Library()
+    library.add_book(Book("Python Crash Course", "Eric Matthes", 2019))
+    library.add_book(Book("Python Hard Way", "Zed Shaw", 2013))
+    library.add_book(Book("Head First Python", "Paul Barry", 2016))
+    books = library.catalog
+    return render_template('index.html', name=name, books=books)
 
 
 if __name__ == '__main__':

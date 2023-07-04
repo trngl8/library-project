3a4b9ab4f413a689746f1009531e1fced9b73e39
Red Rubber Duck <122934358+roman-duck@users.noreply.github.com>
2023-07-04 20:01:29 +0300

    library as menu argument, close #61

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_exception_range(max_value):
     print(f"You should enter a number between 1 and {max_value}")
 
 
-def find_book(library : Library):
+def find_book(library: Library):
     to_find = str(input("Enter title, author or ISBN "))
     list_for_author = []
     list_for_title = []
@@ -21,7 +21,7 @@ def find_book(library : Library):
         if i.isbn == to_find and i.available is True:
             return "There is an available book with such isbn"
     if len(list_for_author) > 0:
-        return  "This author has written the following books: " + str(list_for_author)
+        return "This author has written the following books: " + str(list_for_author)
     if len(list_for_title) > 0:
         return "There are the following books with this title: " + str(list_for_title)
     return "There is no books currently available with this title, author or ISBN"
@@ -31,14 +31,28 @@ def find_user():
     pass
 
 
-def import_books():
+def import_books(library: Library):
     filename = input("Enter filename :> ")
-    filename = "var/data/" + filename 
-    library = Library()
+    filename = "var/data/" + filename
     list_of_books = library.read_from_csv_catalog(filename)
     number_of_imported_books = len(list_of_books)
     library.import_books(list_of_books)
     return f"Imported {number_of_imported_books} books"
+
+
+def get_choice_function(param=None):
+    choices = {
+        "Find a book": find_book,
+        "Find a user": find_user,
+        "Import books": import_books,
+    }
+    return choices.get(param, lambda val: val)
+
+
+def menu_choice():
+    library = Library()
+    choice_function = get_choice_function(menu[choice - 1])
+    return choice_function(library)
 
 
 actions = {
@@ -62,5 +76,4 @@ while choice == 0:
         print_exception_range(len(menu))
         choice = 0
 
-function = actions.get(menu[choice - 1])
-print(function())
+print(menu_choice())

a57f4d44527be113ac17276e9220e167d96dd596
ArtemKrayevskiy <116542338+ArtemKrayevskiy@users.noreply.github.com>
2023-07-04 19:10:46 +0300

    find a book, close #24
    ---------
    
    Co-authored-by: Red Rubber Duck <mvch@triangle.software>

--- main.py
+++ main.py
@@ -9,8 +9,22 @@ def print_exception_range(max_value):
     print(f"You should enter a number between 1 and {max_value}")
 
 
-def find_book():
-    pass
+def find_book(library : Library):
+    to_find = str(input("Enter title, author or ISBN "))
+    list_for_author = []
+    list_for_title = []
+    for i in library.catalog:
+        if i.title == to_find and i.available is True:
+            list_for_title.append(i)
+        if i.author == to_find and i.available is True:
+            list_for_author.append(i)
+        if i.isbn == to_find and i.available is True:
+            return "There is an available book with such isbn"
+    if len(list_for_author) > 0:
+        return  "This author has written the following books: " + str(list_for_author)
+    if len(list_for_title) > 0:
+        return "There are the following books with this title: " + str(list_for_title)
+    return "There is no books currently available with this title, author or ISBN"
 
 
 def find_user():

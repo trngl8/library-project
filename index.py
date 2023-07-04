8ac2d9c054bd0f46e8258687037f0ada0cc45467
ArtemKrayevskiy <kraievskyi.pn@ucu.edu.ua>
2023-07-04 19:59:00 +0300

    Cookies added

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, make_response
 from flask import render_template
 from flask_bootstrap import Bootstrap5
 
@@ -24,6 +24,19 @@ def catalog():
     books = library.catalog
     return render_template('index.html', name=name, books=books)
 
+@app.route('/enter' , methods = ["POST", "GET"])
+def enter():
+    if request.method == "POST":
+        user = request.form["nm"]
+        resp = make_response(render_template("readcookie.html"))
+        resp.set_cookie("userID", user)
+
+        return resp
+
+@app.route("/getcookie")
+def getcookie():
+    name = request.cookies.get("userID")
+    return '<h1>welcome ' + name + '</h1>'
 
 @app.route('/books/<int:book_id>')
 def book(book_id):

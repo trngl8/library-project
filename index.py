3b3bd36550a87302aefcde71e44aec1367eb1684
Red Rubber Duck <mvch@triangle.software>
2023-07-12 10:23:19 +0300

    phone fix

--- index.py
+++ index.py
@@ -15,7 +15,7 @@ class OrderForm(Form):
     firstname = StringField('Firstname', [validators.Length(min=4, max=25)])
     lastname = StringField('Lastname', [validators.Length(min=4, max=25)])
     email = EmailField('Email', [validators.Email()])
-    phone = StringField('Email', [validators.Length(min=6, max=11)])
+    phone = StringField('Phone', [validators.Length(min=6, max=11)])
     address = StringField('Address', [validators.Length(min=6, max=35)])
     period = SelectField('Period', choices=[('2', '2 weeks'), ('4', '4 weeks'), ('16', '16 weeks')])
     accept = BooleanField('I accept agreement', [validators.DataRequired()])

65b0c3071f6ecceb2094df599c72d61a505f5acc
Red Rubber Duck <mvch@triangle.software>
2023-07-16 16:29:01 +0300

    fixed tables structure

--- next/schema.sql
+++ next/schema.sql
@@ -2,9 +2,15 @@ DROP TABLE IF EXISTS orders;
 
 CREATE TABLE orders (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  name TEXT NOT NULL,
+  first_name TEXT NOT NULL,
+  second_name TEXT NOT NULL,
+  email TEXT NOT NULL,
+  phone TEXT NOT NULL,
+  address TEXT NOT NULL,
+  period INTEGER NOT NULL,
+  total_price INTEGER NOT NULL,
+  status TEXT NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  title TEXT NOT NULL
 );
 
 CREATE TABLE books (
@@ -13,5 +19,15 @@ CREATE TABLE books (
   author TEXT NOT NULL,
   publishing_year INTEGER NOT NULL,
   isbn TEXT NOT NULL,
+  available INTEGER NOT NULL,
   created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE books_orders (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  order_id INTEGER NOT NULL,
+  book_id INTEGER NOT NULL,
+  price INTEGER NOT NULL,
+  FOREIGN KEY (order_id) REFERENCES orders (id),
+  FOREIGN KEY (book_id) REFERENCES books (id)
 );

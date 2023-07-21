d74f6a3c67c766a8feb5ca0e7ebfb33af5b29e86
Red Rubber Duck <mvch@triangle.software>
2023-07-21 15:25:25 +0300

    renamed back

--- storage.py
+++ storage.py
@@ -18,7 +18,6 @@ class DataStorage:
         if not os.path.exists("var/data/users.csv"):
             open("var/data/users.csv", 'w')
         self.data = []
-        # self.read_from_csv_catalog(path + "/books.csv")
 
     def save_user(self, user):
         with open("var/data/users.csv", "a") as file_object:
@@ -31,7 +30,7 @@ class DataStorage:
                 return {'name': item[0], 'email': item[1], 'phone': item[2]}
         raise Exception("User not found")
 
-    def find_all_books(self, filename):
+    def read_from_csv_catalog(self, filename):
         try:
             with open(self.path + filename + self.ext, 'r') as file:
                 reader = csv.reader(file)

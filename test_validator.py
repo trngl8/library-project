db3671bad436d9e556756c03d747680f5e03e25f
ArtemKrayevskiy <kraievskyi.pn@ucu.edu.ua>
2023-07-26 12:29:59 +0300

    Validator added

--- test_validator.py
+++ test_validator.py
@@ -1,25 +1,29 @@
 import unittest
-
+from validator import Validator
+from library import Book
 
 class TestValidator(unittest.TestCase):
     def setUp(self):
         self.validator = Validator()
         self.validator.add_rule({
             "ISBN": {
-                "type": "string",
-                "regexp": "(?:ISBN(?:-13)?:? )?(?=[0-9]{13}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)97[89][- ]?[0-9]{1,5}[- ]?(?:[0-9]+[- ]?){2}[0-9X]"
+                "type": str,
+                "regexp": r"^(?:ISBN(?:-13)?:? )?(?=[0-9]{13}$|(?=(?:[0-9]+[- ]){4})[- 0-9]{17}$)97[89][- ]?[0-9]{1,5}[- ]?(?:[0-9]+[- ]?){2}[0-9X]$"
             },
         })
         self.validator.add_rule({
             "year": {
-                "type": "integer",
+                "type": int,
                 "min": 1900,
                 "max": 2050
             },
         })
 
     def test_validate(self):
-        self.assertEqual(True, self.validator.validate())
+        book1 = Book(1, "Python Crash Course", "Eric Matthes" ,2019, "9781718502703")
+        book2 = Book(2,"Python Hard Way", "Zed Shaw", 1899, "9780134692883")
+        self.assertEqual(True, self.validator.validate(book1))
+        self.assertAlmostEqual(False, self.validator.validate(book2))
 
 
 if __name__ == "__main__":

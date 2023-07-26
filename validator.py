3d3797b8569734c6fbe24c1ea27791851d1d2cd0
Red Rubber Duck <mvch@triangle.software>
2023-07-26 19:31:16 +0300

    fixes

--- validator.py
+++ validator.py
@@ -2,7 +2,6 @@ import re
 from library import Book
 
 
-
 class Validator():
     def __init__(self):
         self.rules = {}
@@ -13,7 +12,7 @@ class Validator():
     def validate(self, book : Book):
         if len(self.rules) == 0:
             return True
-        if isinstance(book.isbn, self.rules["ISBN"]["type"]) and re.match(self.rules["ISBN"]["regexp"], book.isbn) and \
+        if isinstance(book.isbn, self.rules["ISBN"]["type"]) and re.match(self.rules["ISBN"]["regexp"], book.isbn) and\
 isinstance(book.year, self.rules["year"]["type"]) and self.rules["year"]["min"]< book.year < self.rules["year"]["max"]:
             return True
         return False

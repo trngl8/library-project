03d26d32ebb0af7fead3d0ab16c5f89829d81240
ArtemKrayevskiy <kraievskyi.pn@ucu.edu.ua>
2023-07-26 15:17:03 +0300

    Empty validator behavior added

--- validator.py
+++ validator.py
@@ -11,6 +11,8 @@ class Validator():
         self.rules[list(rule.keys())[0]] = rule[list(rule.keys())[0]]
 
     def validate(self, book : Book):
+        if len(self.rules) == 0:
+            return True
         if isinstance(book.isbn, self.rules["ISBN"]["type"]) and re.match(self.rules["ISBN"]["regexp"], book.isbn) and \
 isinstance(book.year, self.rules["year"]["type"]) and self.rules["year"]["min"]< book.year < self.rules["year"]["max"]:
             return True

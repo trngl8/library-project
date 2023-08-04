eac0ea842b46d77248dd4db4a093dac77dc6e4b9
Red Rubber Duck <mvch@triangle.software>
2023-08-04 22:15:57 +0300

    storage success

--- storage.py
+++ storage.py
@@ -2,9 +2,24 @@ import os
 import csv
 
 
+class FileLines:
+    def read_lines(self, file_path) -> list:
+        with open(file_path, 'r') as file_object:
+            lines = file_object.read().splitlines()
+        return lines
+
+    def write_lines(self, file_path, lines):
+        with open(file_path, "w") as file_object:
+            file_object.writelines("\n".join(lines))
+
+    def write_line(self, file_path, line):
+        with open(file_path, "a") as file_object:
+            file_object.write(line + "\n")
+
+
 class DataStorage:
 
-    def __init__(self):
+    def __init__(self, file_lines: FileLines):
         struct = {
             'books': [
                 "ID", "TITLE", "AUTHOR", "YEAR", "ISBN", "AVAILABLE", "CREATED"
@@ -28,6 +43,9 @@ class DataStorage:
                 with open(self.path + key + self.ext, 'w') as file_object:
                     file_object.write(",".join(value) + '\n')
         self.data = {}
+        self.counters = {}
+        self.headers = {}
+        self.lines = file_lines
 
     def save_user(self, user):
         with open(self.path + "users.csv", "a") as file_object:
@@ -52,30 +70,36 @@ class DataStorage:
 
     def get_lines(self, entity_name):
         if entity_name in self.data:
-            return self.data[entity_name]['lines']
-        with open(self.path + entity_name + self.ext, 'r') as file_object:
-            lines = file_object.read().splitlines()
-        self.data[entity_name] = {
-            'count': len(lines) - 1,
-            'lines': lines,
-        }
-        return lines
+            return self.data[entity_name]
+        lines = self.lines.read_lines(self.path + entity_name + self.ext)
+        self.headers[entity_name] = lines.pop(0)
+        self.counters[entity_name] = len(lines)
+        self.data[entity_name] = lines
+        return self.data[entity_name]
 
     def add_line(self, entity_name, item) -> int:
         if entity_name not in self.data:
             self.get_lines(entity_name)
-        new_id = self.data[entity_name]['count'] + 1
-        line = str(new_id) + "," + ",".join(item.values()) + "\n"
-        with open(self.path + entity_name + self.ext, "a") as file_object:
-            file_object.write(line)
-        self.data[entity_name]['count'] = new_id
-        self.data[entity_name]['lines'].append(line)
+        new_id = self.counters[entity_name] + 1
+        line = str(new_id) + "," + ",".join(item.values())
+        self.lines.write_line(self.path + entity_name + self.ext, line)
+        self.counters[entity_name] = new_id
+        self.data[entity_name].append(line)
         return new_id
 
     def remove_line(self, entity_name, item_id):
         if entity_name not in self.data:
             self.get_lines(entity_name)
-        self.data[entity_name]['count'] -= 1
-        del self.data[entity_name]['lines'][int(item_id)]
-        with open(self.path + entity_name + self.ext, "w") as file_object:
-            file_object.writelines("\n".join(self.data[entity_name]['lines']))
+        self.counters[entity_name] -= 1
+        del self.data[entity_name][int(item_id) - 1]
+        self.lines.write_lines(self.path + entity_name + self.ext, self.data[entity_name])
+
+    def get_header(self, entity_name):
+        if entity_name not in self.headers:
+            self.get_lines(entity_name)
+        return self.headers[entity_name]
+
+    def get_count(self, entity_name):
+        if entity_name not in self.counters:
+            self.get_lines(entity_name)
+        return self.counters[entity_name]

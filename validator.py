c2165348c8ac6e9916856a30b6fdcd0dddcc72f5
ArtemKrayevskiy <kraievskyi.pn@ucu.edu.ua>
2023-08-09 13:29:02 +0300

    validator improved

--- validator.py
+++ validator.py
@@ -22,22 +22,17 @@ class Validator():
                 isinstance(book.year, self.rules["year"]["type"]) and self.rules["year"]["min"] < book.year < \
                 self.rules["year"]["max"]:
             return True
+        return False
 
     def validate(self, object):
         if isinstance(object, Book):
             return self.validate_book(object)
-
-        for rules in self.rules.values():
-            for rule in rules:
-                # TODO: implement validation behavior
-                result = rule.validate(object)
-
         if len(object) == 0:
-            for i in range(2):
+            for i in range(len(self.rules['email'])):
                 self.errors.append("Validation failed")
             return False
         for i in self.rules.get(list(object.keys())[0]):
-            if not i.validate(object.get(list(object.keys())[0])):
+            if not i.validate(object):
                 self.errors.append("Validation failed")
         if len(self.errors) == 0:
             return True
@@ -46,16 +41,19 @@ class Validator():
 
 
 class Email:
-    def validate(self, email):
+    def validate(self, data):
         if re.match(r"(?!\.)([a-zA-Z0-9!#$%&'*\
 +-/=?^_`{|}~](?!.*\.\.)){1,63}(?<!\.)@[a-z]{1,255}\.(com|or\
 g|edu|gov|net|ua)(\.?)((co\
-m|org|edu|gov|net|ua)?)", email):
+m|org|edu|gov|net|ua)?)", data['email']):
             return True
         else:
             return False
     
 
 class Required:
-    def validate(self, string):
-        return string is not None and string != ""
+    def validate(self, data):
+        for key in data:
+            if data[key] is None or data[key] == '':
+                return False
+        return True

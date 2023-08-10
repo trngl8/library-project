c6c8f31b673a58db82e76003afa0c860214df57f
Red Rubber Duck <122934358+roman-duck@users.noreply.github.com>
2023-08-10 17:31:25 +0300

    validator improvements, close #142
    Co-authored-by: ArtemKrayevskiy <kraievskyi.pn@ucu.edu.ua>

--- test_validator.py
+++ test_validator.py
@@ -1,5 +1,5 @@
 import unittest
-from validator import Validator
+from validator import Validator, Required, Email, Length
 from library import Book
 
 
@@ -25,6 +25,54 @@ class TestValidator(unittest.TestCase):
         book2 = Book("Python Hard Way", "Zed Shaw", 1899, "9780134692883")
         self.assertEqual(True, self.validator.validate(book1))
         self.assertAlmostEqual(False, self.validator.validate(book2))
+
+    def test_validate_email_success(self):
+        validator = Validator()
+        validator.add({'email': [Required(), Email()]})
+        data = {
+            "email": "test@test.com"
+        }
+        result = validator.validate(data)
+        self.assertEqual(True, result)
+        self.assertEqual(0, len(validator.errors))
+
+        data = {
+            "email": "test@triangle.software"
+        }
+        result = validator.validate(data)
+        self.assertEqual(True, result)
+
+    def test_validate_complex_data(self):
+        validator = Validator()
+        validator.add({'name': [Required(), Length(min=3, max=50)]})
+        validator.add({'email': [Required(), Email()]})
+        validator.add({'admin_email': [Email()]})
+        data = {
+            "name": "Test",
+            "email": "test@triangle.software",
+            "admin_email": "admin@triangle.software"
+        }
+        result = validator.validate(data)
+        self.assertEqual(True, result)
+        self.assertEqual(0, len(validator.errors))
+
+    def test_validate_data_empty(self):
+        validator = Validator()
+        validator.add({'email': [Required(), Email()]})
+        data = {}
+        result = validator.validate(data)
+        self.assertEqual(False, result)
+        self.assertEqual(2, len(validator.errors))
+
+    def test_validate_email_invalid(self):
+        validator = Validator()
+        validator.add({'email': [Required(), Email()]})
+        data = {
+            "email": "no_email"
+        }
+        result = validator.validate(data)
+        self.assertEqual(False,result)
+        self.assertEqual(1, len(validator.errors))
 
 
 if __name__ == "__main__":
--- validator.py
+++ validator.py
@@ -5,16 +5,70 @@ from library import Book
 class Validator():
     def __init__(self):
         self.rules = {}
-    
+        self.errors = []
+
+    def add(self, rules: dict):
+        for key in rules.keys():
+            self.rules[key] = rules[key]
+
     def add_rule(self, rule : dict):
         self.rules[list(rule.keys())[0]] = rule[list(rule.keys())[0]]
 
-    def validate(self, book : Book):
+    def validate_book(self, book: Book):
         if len(self.rules) == 0:
             return True
-        if isinstance(book.isbn, self.rules["ISBN"]["type"]) and re.match(self.rules["ISBN"]["regexp"], book.isbn) and\
-isinstance(book.year, self.rules["year"]["type"]) and self.rules["year"]["min"]< book.year < self.rules["year"]["max"]:
+        if isinstance(book.isbn, self.rules["ISBN"]["type"]) and re.match(self.rules["ISBN"]["regexp"],
+                                                                            book.isbn) and \
+                isinstance(book.year, self.rules["year"]["type"]) and self.rules["year"]["min"] < book.year < \
+                self.rules["year"]["max"]:
             return True
         return False
 
+    def validate(self, object):
+        if isinstance(object, Book):
+            return self.validate_book(object)
+        else:
+            if len(object) == 0:
+                checker = []
+                for rule in self.rules:
+                    for i in self.rules[rule]:
+                        if i not in checker:
+                            checker.append(i)
+                self.errors = ["Validation error"]* len(checker)
+                return False
+            for key in object:
+                object_to_check = object[key]
+                rules_to_check = self.rules[key]
+                for i in rules_to_check:
+                    if not i.validate(object_to_check):
+                        self.errors.append("Validation error")
+            if len(self.errors) != 0:
+                return False
+            return True
 
+
+
+class Email:
+    def validate(self, string):
+        if re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', string):
+            return True
+        else:
+            return False
+    
+
+class Required:
+    def validate(self, string):
+        if string is None or string == '':
+            return False
+        return True
+
+
+class Length:
+    def __init__(self, min, max):
+        self.min = min
+        self.max = max
+    
+    def validate(self, string):
+        if self.min <= len(string) <= self.max:
+            return True
+        return False

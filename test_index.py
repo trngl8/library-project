ffcdba29911e828d6d26db49aa5b65be20a83404
Red Rubber Duck <mvch@triangle.software>
2023-08-10 11:58:46 +0300

    added tests

--- test_index.py
+++ test_index.py
@@ -23,6 +23,54 @@ class AppTestCase(unittest.TestCase):
         self.assertEqual(response.content_type, 'text/html; charset=utf-8')
         self.assertEqual(response.mimetype, 'text/html')
 
+    def test_book_route(self):
+        response = self.app.get('/books/1')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'3 Books', response.data)
+        self.assertEqual(response.content_type, 'text/html; charset=utf-8')
+        self.assertEqual(response.mimetype, 'text/html')
+
+    def test_book_order_route(self):
+        response = self.app.get('/books/1/borrow')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'3 Books', response.data)
+        self.assertEqual(response.content_type, 'text/html; charset=utf-8')
+        self.assertEqual(response.mimetype, 'text/html')
+
+    def test_book_order_confirm_route(self):
+        response = self.app.get('/order/1/confirm')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'3 Books', response.data)
+        self.assertEqual(response.content_type, 'text/html; charset=utf-8')
+        self.assertEqual(response.mimetype, 'text/html')
+
+    def test_profile_route(self):
+        response = self.app.get('/profile')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'3 Books', response.data)
+        self.assertEqual(response.content_type, 'text/html; charset=utf-8')
+        self.assertEqual(response.mimetype, 'text/html')
+
+    def test_settings_route(self):
+        response = self.app.get('/settings')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'3 Books', response.data)
+        self.assertEqual(response.content_type, 'text/html; charset=utf-8')
+        self.assertEqual(response.mimetype, 'text/html')
+
+    def test_logout_route(self):
+        response = self.app.get('/logout')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.content_type, 'text/html; charset=utf-8')
+        self.assertEqual(response.mimetype, 'text/html')
+
+    def test_cart_route(self):
+        response = self.app.get('/cart')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'3 Books', response.data)
+        self.assertEqual(response.content_type, 'text/html; charset=utf-8')
+        self.assertEqual(response.mimetype, 'text/html')
+
 
 if __name__ == '__main__':
     unittest.main()

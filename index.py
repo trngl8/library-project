8e23d036ca001443eaca987399aee9763ae42928
Bogdan <109428914+savag21@users.noreply.github.com>
2023-08-14 19:15:20 +0300

    changed cookies for session usage, close #155
    ---------
    
    Co-authored-by: Red Rubber Duck <mvch@triangle.software>

--- index.py
+++ index.py
@@ -44,9 +44,8 @@ def home():
         if not re.match(r"[A-Za-z0-9_-]+", username):
             flash("Your name is not valid", category="error")
             return redirect(url_for("home"))
-        resp = redirect(url_for('index'))
-        resp.set_cookie("SERVER_COOKIE", username)
-        return resp
+        session['username'] = username
+        return redirect(url_for('index'))
 
     return render_template('enter.html', library=library)
 
@@ -67,23 +66,20 @@ def index():
             amount = import_service.process_file(file, filename, library=library)
             flash(f"Your file was imported successfully. {amount} unique books imported", category='success')
             return redirect(url_for('index'))
-    user = request.cookies.get('SERVER_COOKIE')
     books = library.get_repository('books').find_all()
-    resp = make_response(render_template('index.html', books=books, user=user, library=library))
+    resp = make_response(render_template('index.html', books=books, library=library))
     return resp
 
 
 @app.route('/books/<int:book_id>')
 def book(book_id):
-    user = request.cookies.get('SERVER_COOKIE')
     item = library.get_repository('books').find(book_id)
-    resp = make_response(render_template('book.html', book=item, user=user, library=library))
+    resp = make_response(render_template('book.html', book=item, library=library))
     return resp
 
 
 @app.route('/books/<int:book_id>/borrow', methods=["GET", "POST"])
 def order(book_id):
-    user = request.cookies.get('SERVER_COOKIE')
     item = library.get_repository('books').find(book_id)
     form = OrderForm(request.form)
     if request.method == 'POST' and form.validate():
@@ -93,39 +89,34 @@ def order(book_id):
         else:
             flash("Processing failed", category="error")
         return redirect(url_for('confirm', book_id=book_id))
-    resp = make_response(render_template('book_order.html', book=item, form=form, user=user, library=library))
+    resp = make_response(render_template('book_order.html', book=item, form=form, library=library))
     return resp
 
 
 @app.route('/profile')
 def profile():
-    user = request.cookies.get('SERVER_COOKIE')
-    return render_template("profile.html", user=user, library=library)
+    return render_template("profile.html", library=library)
 
 
 @app.route('/settings')
 def settings():
-    user = request.cookies.get('SERVER_COOKIE')
-    return render_template("settings.html", user=user, library=library)
+    return render_template("settings.html", library=library)
 
 
 @app.route("/logout")
 def logout():
     response = make_response(redirect(url_for('home')))
-    response.delete_cookie('SERVER_COOKIE')
     return response
 
 
 @app.route('/order/<int:book_id>/confirm', methods=["GET", "POST"])
 def confirm(book_id):
-    user = request.cookies.get('SERVER_COOKIE')
     item = library.get_repository('books').find(book_id)
-    return make_response(render_template('order_confirm.html', library=library, book=item, user=user))
+    return make_response(render_template('order_confirm.html', library=library, book=item))
 
 
 @app.route('/cart', methods=["GET", "POST"])
 def cart_index():
-    user = request.cookies.get('SERVER_COOKIE')
     cart = library.cart
     if request.method == 'POST':
         cart.clear()
@@ -138,7 +129,7 @@ def cart_index():
         cart_data = session['cart']
         for item in cart_data['items']:
             cart.add_item(item)
-    return make_response(render_template('cart.html', library=library, user=user, cart=cart))
+    return make_response(render_template('cart.html', library=library, cart=cart))
 
 
 @app.route('/cart/<int:book_id>/add', methods=["POST"])

887b2b3558d444197fd4d5b7ea5ed34295a8a161
Red Rubber Duck <mvch@triangle.software>
2023-09-13 19:20:12 +0300

    color form field

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
-from wtforms import Form, BooleanField, StringField, EmailField, SelectField, validators
+from wtforms import Form, BooleanField, StringField, EmailField, SelectField, SelectFieldBase, validators
 
 
 class OrderForm(Form):
@@ -15,6 +15,7 @@ class BookEditForm(Form):
     title = StringField('Title', [validators.Length(min=4, max=25)])
     author = StringField('Author', [validators.Length(min=4, max=25)])
     year = StringField('Year', [validators.InputRequired()])
+    color = SelectFieldBase('Year')
 
 
 class NewNameForm(Form):

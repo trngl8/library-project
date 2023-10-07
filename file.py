c1dd48f420a1272dff269aaab0a19109bce95d54
Red Rubber Duck <122934358+roman-duck@users.noreply.github.com>
2023-10-07 15:32:49 +0300

    fixed import, close #256

--- file.py
+++ file.py
@@ -39,51 +39,23 @@ class FileImport:
                 files.append(entry_path)
         return files
 
-    def get_file_lines(self, filename):
-        with open(self.path + filename, 'r') as file_object:
-            lines = file_object.read().splitlines()
-        return lines
-
     def save_file(self, file, file_name):
         if not os.path.exists(self.path):
             os.makedirs(self.path)
         file.save(os.path.join(self.path, file_name))
-        with open(os.path.join("var/import/" + file_name), 'r') as file:
-            file_containment = file.readlines()
-            return file_containment
-        
-    def write_new_file(self, file_name, result, library,  flag=True):
-        if flag == True:
-            path = "var/import/"
-        else:
-            path = "var/data/"
-        with open(os.path.join(path + file_name), 'w') as file:
-            file.write(result[0])
-            for i in result[1:]:
-                library.storage.add_line("books", {"title" : i.title, "author": i.author, "year" : str(i.year)})
 
     def process_file(self, file, file_name, library):
-        file_containment = self.save_file(file, file_name)
-        result = []
-        result.append(file_containment[0])
-        flag = False
+        self.save_file(file, file_name)
+        lines = FileLines('/var/import/').read_lines(file_name)
         validator = Validator()
-        for num, line in enumerate(file_containment[1:]):
-            line = line.replace('\n', '').split(",")
-            book = {'title' : line[1], 'author' : line[2], "year" : line[3]}
-            book.id = num + 1
-            if validator.validate(object=book):
-                for j in result:
-                    try:
-                        if j == book:
-                            flag = True
-                    except:
-                        continue
-            if not flag:
-                result.append(book)
-            flag = False
-        self.write_new_file(file_name, result, library=library, flag=False)
-        return len(result) - 1
+        counter = 0
+        for i in lines[1:]:
+            line = i.split(",")
+            book = {'title' : line[1], 'author' : line[2], "year": line[3]}
+            if validator.validate(struct=book):
+                library.storage.add_line('books', book)
+                counter += 1
+        return counter
 
     def import_file(self, file_name, library):
         with open(self.path + file_name, "r") as file:
